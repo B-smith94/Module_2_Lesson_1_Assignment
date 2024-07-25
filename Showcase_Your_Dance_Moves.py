ce639a141b89e8086251e0a0f1e01233f915784e
b-smith94 <byrondsmith9@gmail.com>
2024-07-25 15:45:50 -0400

    completed question 1

---
+++ Showcase_Your_Dance_Moves.py
@@ -0,0 +1,13 @@
+weather = "sunny"
+#the indentation in the example was incorrect.  I hadded the indent on the print command lines.
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+
+mood = input("How are you feeling today? ")
+if mood == "happy":
+    print("That's great to hear!")
+if mood == "sad":
+    print("I hope your day gets better!")
