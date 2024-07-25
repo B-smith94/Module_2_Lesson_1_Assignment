8de35634128a6f3ed6c25db9c04ab145f698cccf
b-smith94 <byrondsmith9@gmail.com>
2024-07-25 15:55:40 -0400

    completed question 2

---
+++ Python_Naming_Convention_Practice.py
@@ -0,0 +1,6 @@
+pi_value = 3.14 #The P in "pi" needed to be lowercase
+user_age = 25 #changed to Snake Case in order to keep a consistent naming convention across variables
+user_location = "New York"
+MAX_LIMIT = 1000 #added _ to MAX_LIMIT to closer match the correct convention with the others.  Kept it all uppercase to show that it is a constant.
+
+#nothing needed to be changed about user_location. user_location was already written in Snake Case.
