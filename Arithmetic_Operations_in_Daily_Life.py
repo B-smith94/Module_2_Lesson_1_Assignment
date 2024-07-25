b2a7535c6bcd5d885fff4db718eaae37ac72ae61
b-smith94 <byrondsmith9@gmail.com>
2024-07-25 16:08:01 -0400

    completed task 3

---
+++ Arithmetic_Operations_in_Daily_Life.py
@@ -0,0 +1,14 @@
+#Task 1
+bread_cost = 3.99
+peanut_butter_cost = 2.99
+grape_jelly_cost = 3.99
+
+total_cost = bread_cost + peanut_butter_cost + grape_jelly_cost
+print(total_cost)
+
+#Task 2
+initial_amount = 10000
+fixed_yearly_interest = 0.07
+total_amount = initial_amount + initial_amount * fixed_yearly_interest
+
+print(total_amount)
