57dccecf1b020e4c149b0259b4bd383c7f94e619
b-smith94 <byrondsmith9@gmail.com>
2024-07-25 16:10:29 -0400

    denoted tasks in each question

--- Showcase_Your_Dance_Moves.py
+++ Showcase_Your_Dance_Moves.py
@@ -1,11 +1,12 @@
-weather = "sunny"
+#Task 1
+weather = "rainy"
 #the indentation in the example was incorrect.  I hadded the indent on the print command lines.
 if weather == "sunny":
     print("Wear sunglasses!")
 else:
     print("Take an umbrella!")
 
-
+#Task 2
 mood = input("How are you feeling today? ")
 if mood == "happy":
     print("That's great to hear!")
